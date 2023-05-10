17eda6bde65ba184df4ddba62b89ba0e0a70a442
Sridhar Ratnakumar <srid@srid.ca>
2023-05-10 08:13:14 -0400

    Upgrade inputs, and upstream haskellFlakeProjectModules

--- flake.nix
+++ flake.nix
@@ -5,39 +5,13 @@
     haskell-flake.url = "github:srid/haskell-flake";
   };
   outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } ({ withSystem, ... }: {
+    flake-parts.lib.mkFlake { inherit inputs; } {
       systems = nixpkgs.lib.systems.flakeExposed;
       imports = [
         inputs.haskell-flake.flakeModule
       ];
       perSystem = { self', pkgs, ... }: {
-        haskellProjects.default = {
-          imports = [
-            self.haskellFlakeProjectModules.input
-          ];
-        };
+        haskellProjects.default = { };
       };
-      # TODO: Generalize and move to a new flake-parts module (or haskell-flake)
-      flake.haskellFlakeProjectModules = rec {
-        # Overlay consumed by this flake.
-        input = { pkgs, ... }: {
-          imports = [
-            # Flake input dependencies go here.
-          ];
-          # Haskell overrides go here.
-          overrides = self: super: { };
-        };
-        # Overlay exposed by this flake.
-        output = { pkgs, lib, ... }: withSystem pkgs.system (ctx@{ config, ... }: {
-          # Pass along the dependency overrides to consuming flake.
-          imports = [
-            input
-          ];
-          # Pass local packages, as overlay, to the consuming flake.
-          source-overrides =
-            lib.mapAttrs (name: ks: ks.root)
-              config.haskellProjects.default.packages;
-        });
-      };
-    });
+    };
 }
